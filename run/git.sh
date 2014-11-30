089376df9eb8d2470e294f30cafdf2470153e652
Lars Janssen <lars@fazy.net>
2014-11-30 22:08:17 +0000

    Add Git user/email config, tidy up a bit

--- run/git.sh
+++ run/git.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-if [ ! -f /.root_pw_set ]; then
-        /set_root_pw.sh
-fi
+#!/bin/sh
+
+export HOME=/root
 
 if [ ! -z "$GIT_AUTHOR_NAME" ]; then
         git config --global user.name "$GIT_AUTHOR_NAME"
@@ -11,5 +10,5 @@ if [ ! -z "$GIT_AUTHOR_EMAIL" ]; then
         git config --global user.email "$GIT_AUTHOR_EMAIL"
 fi
 
-exec /usr/sbin/sshd -D
+touch /var/run/git_config_set
 
